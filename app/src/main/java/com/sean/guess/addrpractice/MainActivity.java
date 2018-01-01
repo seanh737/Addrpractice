a12beeb59a9d4968318534fba2d742b25f3d4b19
Sean Hsieh <seanh737@gmail.com>
2018-01-02 06:33:45 +0800

    Initial commit

---
+++ app/src/main/java/com/sean/guess/addrpractice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sean.guess.addrpractice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void address(View view){
+        startActivity(new Intent(this, AddrActivity.class));
+    }
+}
